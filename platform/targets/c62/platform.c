8c4b10a06278aee17c0eeef577544119d8626b41
OE3ANC <mail@oe3anc.at>
2025-02-02 10:42:45 +0100

    device tree updates

--- platform/targets/c62/platform.c
+++ platform/targets/c62/platform.c
@@ -21,7 +21,7 @@
 #include <interfaces/delays.h>
 #include <hwconfig.h>
 
-//#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/gpio.h>
 //#include <zephyr/drivers/sensor.h>
 //#include <zephyr/drivers/uart.h>
 //#include <zephyr/drivers/led_strip.h>
@@ -30,8 +30,8 @@
 #define BUTTON_PTT_NODE DT_NODELABEL(button_ptt)
 
 static const struct gpio_dt_spec button_ptt = GPIO_DT_SPEC_GET_OR(BUTTON_PTT_NODE, gpios, {0});
-static const struct device *const qdec_dev = DEVICE_DT_GET(DT_ALIAS(qdec0));
-static const struct device *const led_dev  = DEVICE_DT_GET(DT_ALIAS(led0));
+//static const struct device *const qdec_dev = DEVICE_DT_GET(DT_ALIAS(qdec0));
+//static const struct device *const led_dev  = DEVICE_DT_GET(DT_ALIAS(led0));
 
 // This is cross-references in keyboard_ttwrplus.c to implement volume control
 // uint8_t volume_level = 125;
@@ -49,10 +49,10 @@ static hwInfo_t hwInfo =
 };
 
 // RGB led color data
-static struct led_rgb led_color = {0};
+//static struct led_rgb led_color = {0};
 
 
-void platform_init()
+void platform_init_csk6()
 {
     return 0;
 }
@@ -79,24 +79,7 @@ uint8_t platform_getVolumeLevel()
 
 int8_t platform_getChSelector()
 {
-    int rc = sensor_sample_fetch(qdec_dev);
-    if (rc != 0)
-    {
-        printk("Failed to fetch sample (%d)\n", rc);
-        return 0;
-    }
-
-    struct sensor_value val;
-    rc = sensor_channel_get(qdec_dev, SENSOR_CHAN_ROTATION, &val);
-    if (rc != 0)
-    {
-        printk("Failed to get data (%d)\n", rc);
-        return 0;
-    }
-
-    // The esp32-pcnt sensor returns a signed 16-bit value: we remap it into a
-    // signed 8-bit one.
-    return (int8_t) val.val1;
+    return 0;
 }
 
 bool platform_getPttStatus()
@@ -112,44 +95,12 @@ bool platform_pwrButtonStatus()
 
 void platform_ledOn(led_t led)
 {
-    int ret = 0;
-
-    switch(led)
-    {
-        case GREEN:
-            led_color.g = 0xff;
-            break;
-        case RED:
-            led_color.r = 0xff;
-            break;
-        default:
-            break;
-    }
-
-    ret = led_strip_update_rgb(led_dev, &led_color, 1);
-    if (ret)
-        printk("couldn't update strip: %d", ret);
+    ;
 }
 
 void platform_ledOff(led_t led)
 {
-    int ret = 0;
-
-    switch(led)
-    {
-        case GREEN:
-            led_color.g = 0x00;
-            break;
-        case RED:
-            led_color.r = 0x00;
-            break;
-        default:
-            break;
-    }
-
-    ret = led_strip_update_rgb(led_dev, &led_color, 1);
-    if (ret)
-        printk("couldn't update strip: %d", ret);
+    ;
 }
 
 void platform_beepStart(uint16_t freq)
