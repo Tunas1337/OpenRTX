886abc999091635224fbb2c3d6faf0f982ea5cf2
OE3ANC <mail@oe3anc.at>
2025-02-03 11:51:46 +0100

    Comment with flash command

--- run_build_c62.sh
+++ run_build_c62.sh
@@ -4,3 +4,7 @@
 
 # Make call via lisa easier
 rm -rf build; meson setup build; meson compile -C build openrtx_c62
+
+# Do this at your own risk! This may brick your device or start a fire!
+# lisa zep exec cskburn -s /dev/ttyUSB0 -C 6 -b 115200 0x000000 build/zephyr/zephyr.hex 
+# Do this at your own risk! This may brick your device or start a fire!
