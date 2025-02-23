8078feee11c12864b194add60d47292040fd00f8
OE3ANC <mail@oe3anc.at>
2025-02-23 15:40:33 +0100

    Zephyr shell enabled, demo code removed

--- platform/drivers/display/display_c62.c
+++ platform/drivers/display/display_c62.c
@@ -1,6 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2023 by Federico Izzo IU2NUO,                           *
- *                         Niccolò Izzo IU2KIN,                            *
+ *   Copyright (C) 2023 by Niccolò Izzo IU2KIN                             *
  *                         Silvano Seva IU2KWO                             *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -17,16 +16,69 @@
  *   along with this program; if not, see <http://www.gnu.org/licenses/>   *
  ***************************************************************************/
 
-#include <zephyr/logging/log.h>
-LOG_MODULE_REGISTER(sample, LOG_LEVEL_INF);
-
-#include <zephyr/device.h>
 #include <zephyr/drivers/display.h>
-#include <zephyr/kernel.h>
-
 #include <interfaces/display.h>
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
 #include <hwconfig.h>
-#include <stddef.h>
+#include <string.h>
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(display, LOG_LEVEL_DBG);
+
+// Display is monochromatic, one bit per pixel
+#define FB_SIZE ((CONFIG_SCREEN_HEIGHT * CONFIG_SCREEN_WIDTH) / 8 + 1)
+
+static const struct device *displayDev;
+static const struct display_buffer_descriptor displayBufDesc =
+{
+    FB_SIZE,
+    CONFIG_SCREEN_WIDTH,
+    CONFIG_SCREEN_HEIGHT,
+    CONFIG_SCREEN_WIDTH,
+};
+
+static uint8_t shadowBuffer[FB_SIZE];
+
+void display_init()
+{
+    LOG_INF("Display init");
+    //displayDev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
+    //sample();
+    //LOG_INF("Display init done");
+}
+
+void display_terminate()
+{
+
+}
+
+void display_renderRows(uint8_t startRow, uint8_t endRow, void *fb)
+{
+    (void) startRow;
+    (void) endRow;
+    (void) fb;
+}
+
+void display_render(void *fb)
+{
+    (void) fb;
+}
+
+void display_setContrast(uint8_t contrast)
+{
+    (void) contrast;
+}
+
+void display_setBacklightLevel(uint8_t level)
+{
+    (void) level;
+}
+
+
+/*
+Demo Code
+*/
 
 enum corner {
 	TOP_LEFT,
@@ -35,10 +87,13 @@ enum corner {
 	BOTTOM_LEFT
 };
 
-typedef void (*fill_buffer)(enum corner corner, uint8_t grey, uint8_t *buf, size_t buf_size);
+typedef void (*fill_buffer)(enum corner corner, uint8_t grey, uint8_t *buf,
+			    size_t buf_size);
 
 
-static void fill_buffer_argb8888(enum corner corner, uint8_t grey, uint8_t *buf, size_t buf_size)
+
+static void fill_buffer_argb8888(enum corner corner, uint8_t grey, uint8_t *buf,
+				 size_t buf_size)
 {
 	uint32_t color = 0;
 
@@ -62,7 +117,8 @@ static void fill_buffer_argb8888(enum corner corner, uint8_t grey, uint8_t *buf,
 	}
 }
 
-static void fill_buffer_rgb888(enum corner corner, uint8_t grey, uint8_t *buf, size_t buf_size)
+static void fill_buffer_rgb888(enum corner corner, uint8_t grey, uint8_t *buf,
+			       size_t buf_size)
 {
 	uint32_t color = 0;
 
@@ -105,14 +161,14 @@ static uint16_t get_rgb565_color(enum corner corner, uint8_t grey)
 		break;
 	case BOTTOM_LEFT:
 		grey_5bit = grey & 0x1Fu;
-		/* shift the green an extra bit, it has 6 bits */
 		color = grey_5bit << 11 | grey_5bit << (5 + 1) | grey_5bit;
 		break;
 	}
 	return color;
 }
 
-static void fill_buffer_rgb565(enum corner corner, uint8_t grey, uint8_t *buf, size_t buf_size)
+static void fill_buffer_rgb565(enum corner corner, uint8_t grey, uint8_t *buf,
+			       size_t buf_size)
 {
 	uint16_t color = get_rgb565_color(corner, grey);
 
@@ -122,7 +178,8 @@ static void fill_buffer_rgb565(enum corner corner, uint8_t grey, uint8_t *buf, s
 	}
 }
 
-static void fill_buffer_bgr565(enum corner corner, uint8_t grey, uint8_t *buf, size_t buf_size)
+static void fill_buffer_bgr565(enum corner corner, uint8_t grey, uint8_t *buf,
+			       size_t buf_size)
 {
 	uint16_t color = get_rgb565_color(corner, grey);
 
@@ -131,7 +188,8 @@ static void fill_buffer_bgr565(enum corner corner, uint8_t grey, uint8_t *buf, s
 	}
 }
 
-static void fill_buffer_mono(enum corner corner, uint8_t grey, uint8_t *buf, size_t buf_size)
+static void fill_buffer_mono(enum corner corner, uint8_t grey, uint8_t *buf,
+			     size_t buf_size)
 {
 	uint16_t color;
 
@@ -147,168 +205,148 @@ static void fill_buffer_mono(enum corner corner, uint8_t grey, uint8_t *buf, siz
 	memset(buf, color, buf_size);
 }
 
-void display_init()
+int sample(void)
 {
-    size_t x;
-    size_t y;
-    size_t rect_w;
-    size_t rect_h;
-    size_t h_step;
-    size_t scale;
-    size_t grey_count;
-    uint8_t *buf;
-    int32_t grey_scale_sleep;
-    const struct device *display_dev;
-    struct display_capabilities capabilities;
-    struct display_buffer_descriptor buf_desc;
-    size_t buf_size = 0;
-    fill_buffer fill_buffer_fnc = NULL;
+	size_t x;
+	size_t y;
+	size_t rect_w;
+	size_t rect_h;
+	size_t h_step;
+	size_t scale;
+	size_t grey_count;
+	uint8_t *buf;
+	int32_t grey_scale_sleep;
+	const struct device *display_dev;
+	struct display_capabilities capabilities;
+	struct display_buffer_descriptor buf_desc;
+	size_t buf_size = 0;
+	fill_buffer fill_buffer_fnc = NULL;
 
-  /* 获取display设备实例 */
-    display_dev = DEVICE_DT_GET(DT_INST(0, sitronix_st7735r));
+	display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
+	if (!device_is_ready(display_dev)) {
+		LOG_ERR("Device %s not found. Aborting sample.",
+			display_dev->name);
 
-    if (display_dev == NULL) {
-        LOG_ERR("Device not found. Aborting sample.");
-        return;
-    }
+		return 0;
+	}
 
-  /* 获取显示功能 */
-    display_get_capabilities(display_dev, &capabilities);
+	LOG_INF("Display sample for %s", display_dev->name);
+	display_get_capabilities(display_dev, &capabilities);
+    LOG_INF("Display x-res: %i y-res: %i", capabilities.x_resolution, capabilities.y_resolution);
 
-    if (capabilities.screen_info & SCREEN_INFO_MONO_VTILED) {
-        rect_w = 16;
-        rect_h = 8;
-    } else {
-        rect_w = 2;
-        rect_h = 1;
-    }
 
-    h_step = rect_h;
-    scale = (capabilities.x_resolution / 8) / rect_h;
+	if (capabilities.screen_info & SCREEN_INFO_MONO_VTILED) {
+		rect_w = 16;
+		rect_h = 8;
+	} else {
+		rect_w = 2;
+		rect_h = 1;
+	}
 
-    rect_w *= scale;
-    rect_h *= scale;
+	h_step = rect_h;
+	scale = (capabilities.x_resolution / 8) / rect_h;
 
-    if (capabilities.screen_info & SCREEN_INFO_EPD) {
-        grey_scale_sleep = 10000;
-    } else {
-        grey_scale_sleep = 100;
-    }
+	rect_w *= scale;
+	rect_h *= scale;
 
-    buf_size = rect_w * rect_h;
+	if (capabilities.screen_info & SCREEN_INFO_EPD) {
+		grey_scale_sleep = 10000;
+	} else {
+		grey_scale_sleep = 100;
+	}
 
-    if (buf_size < (capabilities.x_resolution * h_step)) {
-        buf_size = capabilities.x_resolution * h_step;
-    }
+	buf_size = rect_w * rect_h;
 
-  /* 色块配置 */
-    switch (capabilities.current_pixel_format) {
-    case PIXEL_FORMAT_ARGB_8888:
-        fill_buffer_fnc = fill_buffer_argb8888;
-        buf_size *= 4;
-        break;
-    case PIXEL_FORMAT_RGB_888:
-        fill_buffer_fnc = fill_buffer_rgb888;
-        buf_size *= 3;
-        break;
-    case PIXEL_FORMAT_RGB_565:
-        fill_buffer_fnc = fill_buffer_rgb565;
-        buf_size *= 2;
-        break;
-    case PIXEL_FORMAT_BGR_565:
-        fill_buffer_fnc = fill_buffer_bgr565;
-        buf_size *= 2;
-        break;
-    case PIXEL_FORMAT_MONO01:
-    case PIXEL_FORMAT_MONO10:
-        fill_buffer_fnc = fill_buffer_mono;
-        buf_size /= 8;
-        break;
-    default:
-        LOG_ERR("Unsupported pixel format. Aborting sample.");
-        return;
-    }
+	if (buf_size < (capabilities.x_resolution * h_step)) {
+		buf_size = capabilities.x_resolution * h_step;
+	}
 
-    buf = k_malloc(buf_size);
+	switch (capabilities.current_pixel_format) {
+	case PIXEL_FORMAT_ARGB_8888:
+		fill_buffer_fnc = fill_buffer_argb8888;
+		buf_size *= 4;
+		break;
+	case PIXEL_FORMAT_RGB_888:
+		fill_buffer_fnc = fill_buffer_rgb888;
+		buf_size *= 3;
+		break;
+	case PIXEL_FORMAT_RGB_565:
+		fill_buffer_fnc = fill_buffer_rgb565;
+		buf_size *= 2;
+		break;
+	case PIXEL_FORMAT_BGR_565:
+		fill_buffer_fnc = fill_buffer_bgr565;
+		buf_size *= 2;
+		break;
+	case PIXEL_FORMAT_MONO01:
+	case PIXEL_FORMAT_MONO10:
+		fill_buffer_fnc = fill_buffer_mono;
+		buf_size /= 8;
+		break;
+	default:
+		LOG_ERR("Unsupported pixel format. Aborting sample.");
 
-    if (buf == NULL) {
-        LOG_ERR("Could not allocate memory. Aborting sample.");
-        return;
-    }
+		return 0;
+	}
 
-    (void)memset(buf, 0xFFu, buf_size);
+	buf = k_malloc(buf_size);
 
-    buf_desc.buf_size = buf_size;
-    buf_desc.pitch = capabilities.x_resolution;
-    buf_desc.width = capabilities.x_resolution;
-    buf_desc.height = h_step;
+	if (buf == NULL) {
+		LOG_ERR("Could not allocate memory. Aborting sample.");
 
-  /*整屏填充白色背景*/
-    for (int idx = 0; idx < capabilities.y_resolution; idx += h_step) {
-        display_write(display_dev, 0, idx, &buf_desc, buf);
-    }
+		return 0;
 
-    buf_desc.pitch = rect_w;
-    buf_desc.width = rect_w;
-    buf_desc.height = rect_h;
+	}
 
-  /*左上角填充红色块*/
-    fill_buffer_fnc(TOP_LEFT, 0, buf, buf_size);
-    x = 0;
-    y = 0;
-    display_write(display_dev, x, y, &buf_desc, buf);
+	(void)memset(buf, 0xFFu, buf_size);
 
-  /*右上角填充绿色块*/
-    fill_buffer_fnc(TOP_RIGHT, 0, buf, buf_size);
-    x = capabilities.x_resolution - rect_w;
-    y = 0;
-    display_write(display_dev, x, y, &buf_desc, buf);
+	buf_desc.buf_size = buf_size;
+	buf_desc.pitch = capabilities.x_resolution;
+	buf_desc.width = capabilities.x_resolution;
+	buf_desc.height = h_step;
 
-  /*右下角填充蓝色块*/
-    fill_buffer_fnc(BOTTOM_RIGHT, 0, buf, buf_size);
-    x = capabilities.x_resolution - rect_w;
-    y = capabilities.y_resolution - rect_h;
-    display_write(display_dev, x, y, &buf_desc, buf);
+	for (int idx = 0; idx < capabilities.y_resolution; idx += h_step) {
+		display_write(display_dev, 0, idx, &buf_desc, buf);
+	}
 
-  /* 关闭显示消隐 */
-    display_blanking_off(display_dev);
+	buf_desc.pitch = rect_w;
+	buf_desc.width = rect_w;
+	buf_desc.height = rect_h;
 
-    grey_count = 0;
-    x = 0;
-    y = capabilities.y_resolution - rect_h;
+	fill_buffer_fnc(TOP_LEFT, 0, buf, buf_size);
+	x = 0;
+	y = 0;
+	display_write(display_dev, x, y, &buf_desc, buf);
 
-  /*左下角灰色动态色块*/
-    while (1) {
-        fill_buffer_fnc(BOTTOM_LEFT, grey_count, buf, buf_size);
-        display_write(display_dev, x, y, &buf_desc, buf);
-        ++grey_count;
-        k_msleep(grey_scale_sleep);
-    }
+	fill_buffer_fnc(TOP_RIGHT, 0, buf, buf_size);
+	x = capabilities.x_resolution - rect_w;
+	y = 0;
+	display_write(display_dev, x, y, &buf_desc, buf);
+
+	fill_buffer_fnc(BOTTOM_RIGHT, 0, buf, buf_size);
+	x = capabilities.x_resolution - rect_w;
+	y = capabilities.y_resolution - rect_h;
+	display_write(display_dev, x, y, &buf_desc, buf);
+
+	display_blanking_off(display_dev);
+
+	grey_count = 0;
+	x = 0;
+	y = capabilities.y_resolution - rect_h;
+
+	while (1) {
+		fill_buffer_fnc(BOTTOM_LEFT, grey_count, buf, buf_size);
+		display_write(display_dev, x, y, &buf_desc, buf);
+		++grey_count;
+		k_msleep(grey_scale_sleep);
+#if CONFIG_TEST
+		if (grey_count >= 1024) {
+			break;
+		}
+#endif
+	}
+
+	return 0;
 }
 
-void display_terminate()
-{
-
-}
-
-void display_renderRows(uint8_t startRow, uint8_t endRow, void *fb)
-{
-    (void) startRow;
-    (void) endRow;
-    (void) fb;
-}
-
-void display_render(void *fb)
-{
-    (void) fb;
-}
-
-void display_setContrast(uint8_t contrast)
-{
-    (void) contrast;
-}
-
-void display_setBacklightLevel(uint8_t level)
-{
-    (void) level;
-}
+ /**/
