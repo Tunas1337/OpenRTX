eb9190b92340ceb2ef321fdecae2a30700d6ac33
Andrej <andrej@case.edu>
2025-06-28 23:14:18 -0400

    Many audio fixes

--- platform/drivers-zephyr/baseband/bk4819/bk4819.c
+++ platform/drivers-zephyr/baseband/bk4819/bk4819.c
@@ -224,6 +224,7 @@ void bk4819_int_disable(bk4819_int_t interrupt)
     WriteRegister(BK4819_REG_3F, ReadRegister(BK4819_REG_3F) & (~interrupt));
 }
 
+// NOTE: Frequency is in 10Hz units
 void bk4819_set_freq(uint32_t freq)
 {
     freq = freq / 10; // Convert to 10 Hz units
