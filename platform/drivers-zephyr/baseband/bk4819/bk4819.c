1571089cb152b742c313e627b9ef7c0e4275cb6b
OE3ANC <dev@oe3anc.at>
2026-03-07 22:31:02 +0100

    Readded convert frequency to 10 Hz units in bk4819_set_freq function

--- platform/drivers-zephyr/baseband/bk4819/bk4819.c
+++ platform/drivers-zephyr/baseband/bk4819/bk4819.c
@@ -227,6 +227,7 @@ void bk4819_int_disable(bk4819_int_t interrupt)
 // NOTE: Frequency is in 10Hz units
 void bk4819_set_freq(uint32_t freq)
 {
+    freq = freq / 10; // Convert to 10 Hz units
     WriteRegister(BK4819_REG_39, (freq >> 16) & 0xFFFF);
     WriteRegister(BK4819_REG_38, freq & 0xFFFF);
     bk4819_rx_on();
